3c325c04db0bfaa022afb31238acee7c6f181e45
zaphodikus <zaphodikus@users.noreply.github.com>
2009-12-27 21:51:29 +0000

    GPL v3 license applied to 50% of source files. license.txt file added about box updated to show GPL v3 version number bumped to 8.16 Setup project updated, common system dlls removed from setup.

--- src/mod_RSsim/SimCmdLine.h
+++ src/mod_RSsim/SimCmdLine.h
@@ -1,5 +1,21 @@
 // SimCmdLine.h: interface for the CSimCmdLine class.
 //
+// Mod_RSSim (c) Embedded Intelligence Ltd. 1993,2009
+// AUTHOR: Conrad Braam.  http://www.plcsimulator.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the
+//    License, or (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 //////////////////////////////////////////////////////////////////////
 
 #if !defined(AFX_SIMCMDLINE_H__4A30F9B7_455E_41F3_810A_6072FF55AD2D__INCLUDED_)
